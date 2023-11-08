da256642dc0185d5915c266d09f718cec2bdb16d
Subash Karki <subashkarki68@gmail.com>
2023-11-08 19:24:53 +0545

    fix: locale not working

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ["en", "ne", "rn"],
+  locales: ["ne", "en", "rn"],
 
   // Used when no locale matches
   defaultLocale: "ne",
